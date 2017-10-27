b8ea7b8014c0c36e9d4e1d6ab166bf0691814070
nira <nayzaa2010@hotmail.com>
2017-10-27 16:02:52 +0700

    3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5
    3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5 3.5

--- src/main/java/camt/cbsd/lab05/entity/Student.java
+++ src/main/java/camt/cbsd/lab05/entity/Student.java
@@ -1,9 +1,23 @@
 package camt.cbsd.lab05.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = false)
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
     String studentId;
     String name;
@@ -13,4 +27,17 @@ public class Student {
     boolean feature;
     int penAmount;
     String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
 }
